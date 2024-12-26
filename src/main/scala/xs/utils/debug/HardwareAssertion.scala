0212698bee84b5173c11b83082db88a3550a9d6d
Archer613 <2846509110@qq.com>
2024-12-26 11:01:36 +0800

    feat: HardwareAssertion add user bits

--- src/main/scala/xs/utils/debug/HardwareAssertion.scala
+++ src/main/scala/xs/utils/debug/HardwareAssertion.scala
@@ -7,7 +7,11 @@ import chisel3.util.experimental.BoringUtils
 import xs.utils.FileRegisters
 import chisel3.experimental.{SourceInfo, SourceLine}
 
-class HwAsrtBundle(width:Int) extends Valid[UInt](UInt(width.W))
+class HwAsrtBundle(width:Int) extends Bundle {
+  val valid = Output(Bool())
+  val id    = Output(UInt(width.W))
+  val user  = Output(UInt(10.W))
+}
 
 case class HwAsrtNode (
   assertion: HwAsrtBundle,
@@ -17,33 +21,51 @@ case class HwAsrtNode (
 
 case class DomainInfo(desc: Seq[(Int, String)]) extends IsLookupable
 
-object HardwareError {
-  def apply(cond:Bool, desc:String):Unit = HardwareAssertion(!cond, desc)
-  def apply(cond:Bool, en:Bool, desc:String):Unit = HardwareAssertion(!cond, en, desc)
-}
-
 object HardwareAssertion {
   private var assertId = 0
   private var hwaNodesSeq = Seq[HwAsrtNode]()
   private var topNode:Option[HwAsrtNode] = None
 
-  def apply(cond:Bool, desc:String)(implicit s: SourceInfo):Unit = {
-    // Hardware
-    val assertion = Wire(new HwAsrtBundle(log2Ceil(assertId + 2)))
-    assertion.valid := RegNext(!cond, false.B)
-    assertion.bits := RegEnable(assertId.U, !cond)
-    val node = HwAsrtNode(assertion, Seq((assertId, desc)), 0)
-    hwaNodesSeq = hwaNodesSeq :+ node
-    assertId = assertId + 1
+  /** Checks for a condition to be valid in the circuit at rising clock edge
+   * when not in reset. If the condition evaluates to false, the circuit
+   * simulation stops with an error. The assert id and user bits will be
+   * output to the module interface
+   *
+   * @param cond condition, assertion fires (simulation fails) when false
+   * @param desc optional format string to print when the assertion fires
+   * @param user optional bits to output some message to module interface
+   * @note desc must be defined as Printable(e.g. cf"xxx") to print chisel-type values
+   * @note user bits total width cant be over 16.W
+   */
+  def apply(cond:Bool, desc:Printable, user:UInt*)(implicit s: SourceInfo):Unit = {
     // EDA
     val debugInfo = s match {
-      case SourceLine(filename, line, col) => s"[$filename:$line:$col]: "
-      case _ => ""
+      case SourceLine(filename, line, col) => cf"$filename:$line:$col: "
+      case _ => cf""
     }
     assert(cond, debugInfo + desc)
+    // Hardware
+    val assertion = Wire(new HwAsrtBundle(log2Ceil(assertId + 2)))
+    val _user = Cat(user.reverse)
+    require(_user.getWidth <= assertion.user.getWidth, s"The bits width of the args([UInt[${_user.getWidth}.W]]) exceeds the upper limit(UInt[${assertion.user.getWidth}.W])")
+    assertion.valid := RegNext(!cond, false.B)
+    assertion.id    := RegEnable(assertId.U, !cond)
+    assertion.user  := RegEnable(_user, !cond)
+    val node = HwAsrtNode(assertion, Seq((assertId, desc.toString)), 0)
+    hwaNodesSeq = hwaNodesSeq :+ node
+    assertId = assertId + 1
   }
 
-  def apply(cond:Bool, en:Bool, desc:String)(implicit s: SourceInfo):Unit = apply(Mux(en, cond, true.B), desc)
+  /** Apply an assertion in the hardware design with an enable signal.
+   *
+   * @param cond condition, assertion fires (simulation fails) when false
+   * @param en   enable signal for the assertion
+   * @param desc optional format string to print when the assertion fires
+   * @param user optional bits to output some message to module interface
+   * @note desc must be defined as Printable(e.g. cf"xxx") to print chisel-type values
+   * @note user bits total width cant be over 16.W
+   */
+  def withEn(cond:Bool, en:Bool, desc:Printable, user:UInt*)(implicit s: SourceInfo):Unit = apply(Mux(en, cond, true.B), desc, Cat(user.reverse))
 
   def placePipe(level:Int, moduleTop:Boolean = false):HwAsrtNode = {
     val children = hwaNodesSeq.filter(_.level < level)
@@ -58,7 +80,8 @@ object HardwareAssertion {
     }
     val desc = children.flatMap(_.desc)
     val valid = Cat(hwaBds.map(_.valid)).orR
-    val id = PriorityMux(hwaBds.map(_.valid), hwaBds.map(_.bits))
+    val id = PriorityMux(hwaBds.map(_.valid), hwaBds.map(_.id))
+    val user = PriorityMux(hwaBds.map(_.valid), hwaBds.map(_.user))
     val node = HwAsrtNode(assertion, desc, level)
     if(!moduleTop) {
       hwaNodesSeq = hwaNodesSeq :+ node
@@ -66,7 +89,8 @@ object HardwareAssertion {
       assertId = 0
     }
     assertion.valid := RegNext(valid, false.B)
-    assertion.bits := RegEnable(id, valid)
+    assertion.id    := RegEnable(id, valid)
+    assertion.user  := RegEnable(user, valid)
     node
   }
 
@@ -74,7 +98,8 @@ object HardwareAssertion {
     val maxId = dinfo.desc.map(_._1).max + assertId
     val newAssertion = Wire(new HwAsrtBundle(log2Ceil(maxId + 2)))
     newAssertion.valid := RegNext(aio.valid, false.B)
-    newAssertion.bits := RegEnable(aio.bits + assertId.U, aio.valid)
+    newAssertion.id := RegEnable(aio.id + assertId.U, aio.valid)
+    newAssertion.user := RegEnable(aio.user, aio.valid)
     val newDesc = dinfo.desc.map(d => (d._1 + assertId, domainName + " " + d._2))
     val node = HwAsrtNode(assertion = newAssertion, desc = newDesc, level = level)
     hwaNodesSeq = hwaNodesSeq :+ node
