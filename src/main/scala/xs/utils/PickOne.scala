b5d886313253b8d2e2ab7f7e09b6988c95d26435
梁森 Liang Sen <liangsen20z@ict.ac.cn>
2025-03-13 10:24:42 +0800

    feat(pick-one): use faster algo to pick-one func.

--- src/main/scala/xs/utils/PickOne.scala
+++ src/main/scala/xs/utils/PickOne.scala
@@ -1,48 +1,21 @@
 package xs.utils
 import chisel3._
+import chisel3.experimental.SourceLine
 import chisel3.util._
-class IdxGenerator(entryNum:Int) extends Module {
-  private val idxWidth = entryNum
-  val io = IO(new Bundle {
-    val entriesValidBitVec = Input(UInt(entryNum.W))
-    val entryIndexOH = Valid(UInt(idxWidth.W))
-  })
-  private val idxOHList = Seq.tabulate(entryNum)(idx => UIntToOH(idx.U, idxWidth))
-  private val candidates = io.entriesValidBitVec.asBools zip idxOHList
-  private def validMux(a:(Bool,UInt), b:(Bool,UInt)) : (Bool,UInt) = {
-    val resData = Mux(a._1, a._2, b._2)
-    val valid = a._1 | b._1
-    (valid, resData)
-  }
-  private val res = ParallelOperation(candidates, validMux)
-  io.entryIndexOH.valid := res._1
-  io.entryIndexOH.bits := res._2
 
-  assert(Mux(io.entryIndexOH.valid, (io.entryIndexOH.bits & io.entriesValidBitVec).orR, true.B))
+object PickOneHigh {
+  def apply(in:UInt)(implicit valName: sourcecode.Name):Valid[UInt] = {
+    val res = Wire(Valid(UInt(in.getWidth.W)))
+    res.valid := in.orR
+    res.bits := in & ((~in).asUInt + 1.U)
+    res.suggestName(valName.value)
+    res
+  }
+  def apply(in: Seq[Bool])(implicit valName: sourcecode.Name): Valid[UInt] = apply(Cat(in.reverse))(valName)
 }
 
-object PickOneHigh{
-  def apply(in:UInt):Valid[UInt] = {
-    val entryIdxGenerator = Module(new IdxGenerator(in.getWidth))
-    entryIdxGenerator.io.entriesValidBitVec := in
-    entryIdxGenerator.io.entryIndexOH
-  }
-  def apply(in: Seq[Bool]): Valid[UInt] = {
-    val entryIdxGenerator = Module(new IdxGenerator(in.length))
-    entryIdxGenerator.io.entriesValidBitVec := Cat(in.reverse)
-    entryIdxGenerator.io.entryIndexOH
-  }
-}
-object PickOneLow{
-  def apply(in: UInt): Valid[UInt] = {
-    val entryIdxGenerator = Module(new IdxGenerator(in.getWidth))
-    entryIdxGenerator.io.entriesValidBitVec := ~in
-    entryIdxGenerator.io.entryIndexOH
-  }
+object PickOneLow {
+  def apply(in: UInt)(implicit valName: sourcecode.Name): Valid[UInt] = PickOneHigh((~in).asUInt)(valName)
 
-  def apply(in: Seq[Bool]): Valid[UInt] = {
-    val entryIdxGenerator = Module(new IdxGenerator(in.length))
-    entryIdxGenerator.io.entriesValidBitVec := ~Cat(in.reverse)
-    entryIdxGenerator.io.entryIndexOH
-  }
+  def apply(in: Seq[Bool])(implicit valName: sourcecode.Name): Valid[UInt] = apply(Cat(in.reverse))(valName)
 }
