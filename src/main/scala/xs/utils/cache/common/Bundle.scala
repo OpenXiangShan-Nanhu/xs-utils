882d3b7c2a0a1f508a86dcc1ee05738a5948bde9
zhuhang <877093922@qq.com>
2025-04-28 09:03:48 +0800

    chore: move PrefetchCtrlFromCore dependency from CPL2 to xs-utilsx

--- src/main/scala/xs/utils/cache/common/Bundle.scala
+++ src/main/scala/xs/utils/cache/common/Bundle.scala
@@ -38,3 +38,37 @@ class TPmetaResp(hartIdLen: Int, fullAddressBits: Int, offsetBits: Int) extends 
   val hartid = UInt(hartIdLen.W)
   val rawData = Vec(log2Floor(512 / (fullAddressBits - offsetBits)), UInt((fullAddressBits - offsetBits).W))
 }
+
+class PrefetchCtrlFromCore extends Bundle {
+  val l2_pf_master_en = Bool()
+  val l2_pf_recv_en = Bool()
+  val l2_pbop_en = Bool()
+  val l2_vbop_en = Bool()
+  val l2_tp_en = Bool()
+}
+class PrefetchCtrl extends Bundle {
+  val l1I_pf_enable = Bool()
+  val l2_pf_enable = Bool()
+  val l1D_pf_enable = Bool()
+  val l1D_pf_train_on_hit = Bool()
+  val l1D_pf_enable_agt = Bool()
+  val l1D_pf_enable_pht = Bool()
+  val l1D_pf_active_threshold = UInt(4.W)
+  val l1D_pf_active_stride = UInt(6.W)
+  val l1D_pf_enable_stride = Bool()
+  val l2_pf_store_only = Bool()
+  val l2_pf_recv_enable = Bool()
+  val l2_pf_pbop_enable = Bool()
+  val l2_pf_vbop_enable = Bool()
+  val l2_pf_tp_enable = Bool()
+
+  def toL2PrefetchCtrl(): PrefetchCtrlFromCore = {
+    val res = Wire(new PrefetchCtrlFromCore)
+    res.l2_pf_master_en := l2_pf_enable
+    res.l2_pf_recv_en := l2_pf_recv_enable
+    res.l2_pbop_en := l2_pf_pbop_enable
+    res.l2_vbop_en := l2_pf_vbop_enable
+    res.l2_tp_en := l2_pf_tp_enable
+    res
+  }
+}
